edeccbb3f5f2baf27b77d379c53263c026fed7f3
KamilFor <67376189+KamilFor@users.noreply.github.com>
2020-07-01 19:52:02 +0200

    Zadanie 6.4

--- js/script.js
+++ js/script.js
@@ -19,10 +19,8 @@ const titleClickHandler = function(event) {
   /* add class 'active' to the clicked link */
   console.log("clickedElement:", clickedElement);
   clickedElement.classList.add("active");
-
   /* DONE remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts article.active");
-
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
   }
@@ -35,6 +33,39 @@ const titleClickHandler = function(event) {
   /* add class 'active' to the correct article */
   targetArticle.classList.add("active");
 };
+
+const optArticleSelector = ".post",
+  optTitleSelector = ".post-title",
+  optTitleListSelector = ".titles";
+
+function generateTitleLinks() {
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log("titleList:", titleList);
+  titleList.innerHTML = "";
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = "";
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+    console.log("id:", articleId);
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log("articleTitle:", articleTitle);
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + "</span></a></li>";
+    console.log("link:", linkHTML);
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log("html:", linkHTML);
+  }
+
+  titleList.innerHTML = html;
+}
+generateTitleLinks();
 
 const links = document.querySelectorAll(".titles a");
 
